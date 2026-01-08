d3b0f1c25794f08abbd1839e58f5d367037756e6
Seoyeon HEO <best3487@snu.ac.kr>
2026-01-08 16:54:10 +0900

    fix: ci run - node version in workflow files, biome error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement === null) {
