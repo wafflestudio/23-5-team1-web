6827b9d55a4b4fec1801e06eda39f63a7a501c14
Seoyeon HEO <best3487@snu.ac.kr>
2026-01-13 12:18:19 +0900

    fix: server health check & import directory, pull conflicts, biome formats

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { AuthProvider } from "./contexts/AuthProvider";
 import { EventProvider } from "./contexts/EventContext";
 import { UserDataProvider } from "./contexts/UserDataContext";
+<<<<<<< HEAD
 import AppRoutes from "./router/AppRoutes";
+=======
+import Home from "./pages/auth/Home";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/Signup/SignUp";
+>>>>>>> 0d2a270 (fix: server health check & import directory, pull conflicts, biome formats)
 
 function App() {
 	return (
 		<AuthProvider>
 			<EventProvider>
 				<UserDataProvider>
+<<<<<<< HEAD
 					<AppRoutes />
+=======
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/auth/login" element={<Login />} />
+						<Route path="/auth/signup" element={<SignUp />} />
+					</Routes>
+>>>>>>> 0d2a270 (fix: server health check & import directory, pull conflicts, biome formats)
 				</UserDataProvider>
 			</EventProvider>
 		</AuthProvider>
--- src/pages/auth/Home.tsx
+++ src/pages/auth/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 
 const {
 	VITE_KAKAO_REST_API_KEY,
