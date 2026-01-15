96428425a04619104621854aba00083b2177dfc2
Seoyeon HEO <best3487@snu.ac.kr>
2026-01-15 17:32:15 +0900

    feat: add yyyy.mm.dd format transform function

--- src/util/Calendar/dateFormatter.ts
+++ src/util/Calendar/dateFormatter.ts
@@ -6,3 +6,11 @@ export const formatDateToYYYYMMDD = (date: Date): string => {
 
 	return `${year}-${month}-${day}`;
 };
+
+export const formatDateDotParsed = (date: Date): string => {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2, "0");
+
+	return `${year}.${month}.${day}`;
+};
